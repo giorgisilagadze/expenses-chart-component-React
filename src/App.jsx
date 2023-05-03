0389cf066a510ceeba92a9f8aed8a3cbdedfb707
giorgisilagadze <silagadze.gio123@gmail.com>
2023-05-03 18:13:32 +0400

    style: create global-styles

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { GlobalStyled } from "./styled-components/Global.Styled";
+
+function App() {
+  return (
+    <>
+      <GlobalStyled />
+    </>
+  );
+}
+
+export default App;
---
+++ src/styled-components/Global.Styled.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+
+    * {
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+        font-family: 'DM Sans', sans-serif;
+    }
+
+    body {
+        height: 100vh;
+        background-color: #f8e9dd;
+    }
+`
